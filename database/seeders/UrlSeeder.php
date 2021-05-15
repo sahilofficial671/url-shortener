e95815943e4dfc17fb018577cb693c6fcb6e8601
Sahil Bhatia <sahilofficial671@gmail.com>
2021-05-15 10:50:34 +0530

    Added sameple hellow world url in seeder

--- database/seeders/UrlSeeder.php
+++ database/seeders/UrlSeeder.php
@@ -34,5 +34,17 @@ class UrlSeeder extends Seeder
             'alias' => 'india',
             'status' => true
         ]);
+
+
+        Url::create([
+            'protocol' => 'https',
+            'domain' => 'www.google.com',
+            'path' => '/search',
+            'query' => 'q=hello+world&sxsrf=ALeKk01kLVVuScZNI1CurW0px6zRWtxvaw%3A1621055778102&source=hp&ei=IlmfYOWsA7KcmgfbgZbwDQ',
+            'max_hits' => 120,
+            'hits' => 0,
+            'alias' => 'goo234',
+            'status' => true
+        ]);
     }
 }
